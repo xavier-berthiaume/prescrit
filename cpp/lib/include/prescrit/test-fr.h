3ed6cf84014a88d6cd5519e21a03e57845b01ca6
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2024-04-29 10:54:42 -0400

    Creating a more static implementation with fewer dependencies

--- cpp/lib/include/prescrit/test-fr.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_FR_H
-#define TEST_FR_H
-
-#include "testing/products-tf.h"
-#include "testing/prescriber-tf.h"
-#include "testing/prescription-tf.h"
-#include "testing/patient-tf.h"
-
-#endif
